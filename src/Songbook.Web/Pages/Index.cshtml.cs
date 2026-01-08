1e2da9c505caecb13378652a9c28c4c4a1070668
Konstantin <konstantin.sittner@mailbox.tu-dresden.de>
2026-01-08 12:20:56 +0100

    Refactor Index page layout and remove unused code

--- src/Songbook.Web/Pages/Index.cshtml.cs
+++ src/Songbook.Web/Pages/Index.cshtml.cs
@@ -1,25 +1,14 @@
 using Microsoft.AspNetCore.Mvc.RazorPages;
-using Microsoft.EntityFrameworkCore;
+/* using Microsoft.EntityFrameworkCore;
 using Songbook.Web.Data;
-using Songbook.Web.Models;
+using Songbook.Web.Models; */
 
 namespace Songbook.Web.Pages;
 
 public class IndexModel : PageModel
 {
-    private readonly SongbookDbContext _context;
-
-    public IndexModel(SongbookDbContext context)
+    public void OnGet()
     {
-        _context = context;
-    }
-
-    public List<Song> Songs { get; set; } = new();
-
-    public async Task OnGetAsync()
-    {
-        Songs = await _context.Songs
-            .Include(s => s.Artist)
-            .ToListAsync();
+        
     }
 }
