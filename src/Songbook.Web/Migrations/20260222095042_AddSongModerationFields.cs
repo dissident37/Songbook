6182e6fb4829b435ff224fce277ceb4a412da7d7
Konstantin <konstantin.sittner@mailbox.tu-dresden.de>
2026-02-22 11:01:40 +0100

    Add moderation fields to Song model: HiddenReason and IsHiddenByAdmin

---
+++ src/Songbook.Web/Migrations/20260222095042_AddSongModerationFields.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Songbook.Web.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddSongModerationFields : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "HiddenReason",
+                table: "Songs",
+                type: "text",
+                nullable: true);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsHiddenByAdmin",
+                table: "Songs",
+                type: "boolean",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "HiddenReason",
+                table: "Songs");
+
+            migrationBuilder.DropColumn(
+                name: "IsHiddenByAdmin",
+                table: "Songs");
+        }
+    }
+}
